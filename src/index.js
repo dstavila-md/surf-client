3375a5fea41a1ef65d01d1f58ea914fcb7f6fe78
XenderMd <denis.stavilamd@gmail.com>
2025-10-05 15:16:24 +0300

    finished reducer setup

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './components/store/reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 const el = document.getElementById('root');
 
