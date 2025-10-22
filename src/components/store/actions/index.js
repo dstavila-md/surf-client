19bc2cfa3d179d6249df06c02f5a7afffde3ad36
XenderMd <denis.stavilamd@gmail.com>
2025-10-22 15:53:48 +0300

    reusing action types

--- src/components/store/actions/index.js
+++ src/components/store/actions/index.js
@@ -6,8 +6,17 @@ const fetchUser = () => async (dispatch) => {
     const response = await axios.get('/api/current-user');
     await dispatch({ type: FETCH_USER, payload: response.data });
   } catch (error) {
-    console.log('error', error);
+    console.log('user fetch error', error);
   }
 };
 
-export { fetchUser };
+const handleToken = (token) => async (dispatch) => {
+  try {
+    const res = await axios.post('/api/stripe', token);
+    dispatch({ type: FETCH_USER, payload: res.data });
+  } catch (error) {
+    console.log('payments error', error);
+  }
+};
+
+export { fetchUser, handleToken };
